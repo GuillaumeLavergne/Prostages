880fd0fed3e0fb53ab22aa2808292cf93e44d9ee
Guigui laverge <glavergne001@iutbayonne.univ-pau.fr>
2022-01-05 15:23:36 +0100

    Quasi rien

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,29 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Formation;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //creation de générateur de données Faker
+        $faker= \Faker\Factory::create('fr_FR');
+        $Dico=array()
+        $DUT = new Formation();
+        $DUT->setNomCourt("DUT");
+        $DUT->setNomLong("Diplome Univeristaire de Technologie");
+
+        $A=$faker->randomElement();
+        $B=$faker->realText(10);
+        $C=$faker->realText(10);
+
+        $ABC= new Formation();
+        $ABC->setNomCourt($A[0].$B[0].$C[0]);
+        $ABC->setNomLong($A." ".$B." ".$C);
+
+        $manager->persist($DUT);
+        $manager->persist($ABC);
 
         $manager->flush();
     }
