a971ecefa1768cd7c3613d1408ef58760a23347d
Guigui laverge <glavergne001@iutbayonne.univ-pau.fr>
2022-01-05 21:04:16 +0100

    Finition de la base de données, remplissage des stages

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -18,23 +18,33 @@ class AppFixtures extends Fixture
         //
         //DECLARATION DES DIFFERENTES FORMATIONS
         //
-        $DUT = new Formation();
+
+        $formations=array();//liste compléte des formations
+
+        $DUT = new Formation();//on créer le DUT
         $DUT->setNomCourt("DUT");
         $DUT->setNomLong("Diplome Univeristaire de Technologie");
+        array_push($formations,$DUT);
 
-        for($i=0;$i<15;$i++)
+        $manager->persist($DUT);
+
+
+        for($i=0;$i<15;$i++)//on génére aléatoirement des formations
         {   
             $A=$faker->word();
             $B=$faker->word();
             $C=$faker->word();
 
-            $ABC= new Formation();
-            $ABC->setNomCourt(strtoupper($A[0]).strtoupper($B[0]).strtoupper($C[0]));
-            $ABC->setNomLong($A." ".$B." ".$C);
-            $manager->persist($ABC);
+            $formation= new Formation();
+            $formation->setNomCourt(strtoupper($A[0]).strtoupper($B[0]).strtoupper($C[0]));
+            $formation->setNomLong($A." ".$B." ".$C);
+
+            array_push($formations,$formation);
+
+            $manager->persist($formation);
 
         }
-        $manager->persist($DUT);
+        
 
         //
         //DECLARATIONS DES ENTREPRISES
@@ -44,7 +54,13 @@ class AppFixtures extends Fixture
                             "Vendeur de tapis éléctroniques",
                             "Vendeur de chouchoux automatisés sur plage et balcons",
                             "Vendeur de tronconneuse roses",
-                            "Vendeurs d'enfants magnétiques IA");
+                            "Vendeurs d'enfants magnétiques IA",
+                            "Createurs d'animatronic",
+                            "Chercheurs quantiques",
+                            "Modelisation de metadonnées",
+                            "Statisticiens et analystes informatique");
+
+        $entreprises= array();//liste compléte des entreprises
 
         for($i=1;$i<15;$i++)
         {
@@ -54,7 +70,67 @@ class AppFixtures extends Fixture
             $entreprise->setAdresse($faker->address());
             $entreprise->setActivite($activites[$faker->numberBetween(0,count($activites)-1)]);
             $entreprise->setUrl("https://".$entreprise->getNom().".com");
+
+            array_push($entreprises,$entreprise);
+
             $manager->persist($entreprise);
+        }
+
+
+        //
+        //DECLARATION DES STAGES
+        //
+        $metier= array("Developpeur","Programmeur","Designer","Statisticien","Analyste","Informaticien","Codeur","Concepteur");
+        $language = array("C","C++","C#","JAVA","JAVASCRIPT","BASH","UML","CSS","PHP","HTML","SQL","Python","R","Fortran");
+        $logiciel = array("Rstudio","Eclipse","Modelio","Visual Studio Code","Balsamiq","Code:blocks","Symfony","Spider");
+        $plateforme = array("Linux","Unix","Windows");
+        $periode = array("an(s)","minute(s)","heure(s)","seconde(s)","jour(s)","mois","semaine(s)");
+
+        for($i=1;$i<75;$i++)//boucle de generation de stage
+        {
+            $stage= new Stage();
+
+            //Generation des variables aleatoires
+            $metierStage=$metier[$faker->numberBetween(0,count($metier)-1)];
+            $languageStage=$language[$faker->numberBetween(0,count($language)-1)];
+            $titreStage=$metierStage." en ".$languageStage;
+
+            $stage->setTitre($titreStage);
+            $stage->setMission($titreStage." sur le logiciel ".$logiciel[$faker->numberBetween(0,count($logiciel)-1)]." sous ".$plateforme[$faker->numberBetween(0,count($plateforme)-1)].", pour une durée de ".$faker->numberBetween(0,12)." ".$periode[$faker->numberBetween(0,count($periode)-1)]);
+            $stage->setEmail($faker->email());
+
+            $stage->setEntreprise($entreprises[$faker->numberBetween(0,count($entreprises)-1)]);
+
+            $nbFormations=$faker->numberBetween(1,count($formations)-1);
+            $formationsDejaSelectionnees = array();
+
+            for($y=0;$y<=$nbFormations;$y++)//boucle d'ajout des formations
+            {
+                $boolean="FALSE";
+
+                $formationTiree=$faker->numberBetween(0,count($formations)-1);
+
+                for($z=0;$z<count($formationsDejaSelectionnees);$z++)//boucle de verification des formations deja ajoutées
+                {
+                    if($formationsDejaSelectionnees[$z]==$formationTiree)
+                    {
+                        $boolean = "TRUE";
+                    }
+                }
+
+                if($boolean=="FALSE")
+                {
+                    array_push($formationsDejaSelectionnees,$formationTiree);
+                    $stage->addFormation($formations[$formationTiree]);
+                }
+                else
+                {
+                    $y--;
+                }
+
+            }
+
+            $manager->persist($stage);
         }
 
         $manager->flush();
