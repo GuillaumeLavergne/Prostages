bb7376826bde6dec44a6c826aa0df74207906785
Guigui laverge <glavergne001@iutbayonne.univ-pau.fr>
2022-01-06 17:25:26 +0100

    Realisation de la page de formation

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\formations;
+use App\Entity\Formation;
 
 class FormationsController extends AbstractController
 {
