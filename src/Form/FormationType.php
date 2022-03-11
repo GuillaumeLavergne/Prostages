2f273cd4dd56716a702e76f51ec7c9da8a9b23e9
Guigui laverge <glavergne001@iutbayonne.univ-pau.fr>
2022-03-11 23:12:06 +0100

    Utilisation du CRUD pour la formation et remplacement des chemins statics par des paths

---
+++ src/Form/FormationType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Form\StageType;
+use App\Entity\Stage;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomCourt')
+            ->add('nomLong')
+            ->add('stage',EntityType::class, array(
+                    'class'=>Stage::class ,
+                    'choice_label'=>'titre',
+                    'multiple'=>true,
+                    'expanded'=>true
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
